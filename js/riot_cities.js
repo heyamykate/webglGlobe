ac398d48e7270161a81b98ffb3d7337705b1f82c
Amy-Kate Andrews <amy-kate.andrews@wearetopsecret.com>
2016-11-09 16:28:43 -0700

    adding initial files

---
+++ js/riot_cities.js
@@ -0,0 +1,11 @@
+var cities = new Array(
+  // mid-season invitational in Braizl
+{ name:'Rio de Janeiro', lat:19.5, lng:-99.1166667 },
+// world championship in China
+{ name: 'Wuhan', lat: 0, lng: 0 },
+{ name: 'Guangzhou', lat: 0, lng: 0 },
+{ name: 'Shanghai', lat: 0, lng: 0 },
+{ name: 'Beijing', lat: 0, lng: 0 },
+// all-star event in australia
+{ name: 'Sydney', lat: 0, lng: 0 }
+);
